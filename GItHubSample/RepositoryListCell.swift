de655ef022b9b44317968bf56b0e9b14f6e73f29
hemg2 <hemg2@naver.com>
2023-01-03 20:53:13 +0900

    UI

--- GItHubSample/RepositoryListCell.swift
+++ GItHubSample/RepositoryListCell.swift
@@ -5,4 +5,24 @@
 //  Created by 1 on 2023/01/03.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class RepositoryListCell: UITableViewCell {
+    var repository: String?
+    
+    
+    let nameLabel = UILabel()
+    let descriptionLabel = UILabel()
+    let starImageView = UIImageView()
+    let starLabel = UILabel()
+    let languageLabel = UILabel()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        [nameLabel, descriptionLabel, starImageView, starLabel, languageLabel].forEach {
+            contentView.addSubview($0)
+        }
+    }
+}
--- GItHubSample/RepositoryListViewController.swift
+++ GItHubSample/RepositoryListViewController.swift
@@ -10,11 +10,41 @@ import RxSwift
 
 class RepositoryListViewController: UITableViewController {
 
+    private let organization = "Apple"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        title = organization + "Repositories"
+        
+        self.refreshControl = UIRefreshControl()
+        let refreshControl = self.refreshControl!
+        refreshControl.backgroundColor = .white
+        refreshControl.tintColor = .darkGray
+        refreshControl.attributedTitle = NSAttributedString(string: "당겨서 새로고침")
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        
+        tableView.register(RepositoryListCell.self, forCellReuseIdentifier: "RepositoryListCell")
+        tableView.rowHeight = 140
     }
-
-
+    
+    @objc func refresh() {
+        
+    }
 }
 
+
+
+extension RepositoryListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryListCell") as? RepositoryListCell else { return RepositoryListCell() }
+        
+        return cell
+    }
+}
